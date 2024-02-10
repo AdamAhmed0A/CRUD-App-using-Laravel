cb97b6015e8ae287c26a5d6b99266acb69e3cd39
Adam Ahmed <130517274+Frisky0A@users.noreply.github.com>
2024-02-10 19:02:19 +0200

    web.php

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-/// showing a specific page from Testcontroller 
-
+// Routes
 Route::controller(UserController::class)->group(function() {
     Route::get("/", "index");
 
